9b052c7793fe8f90d7d551959c887ec8469ab12a
ilcapone <juanzumbido@hotmail.com>
2016-12-13 00:20:08 +0100

    First Commit

---
+++ R/XMLParseCPE.R
@@ -0,0 +1,21 @@
+
+
+#install.packages("XML")
+#install.packages("plyr")
+#install.packages("ggplot2")
+#install.packages("gridExtra")
+
+#require("XML")
+#require("plyr")
+#require("ggplot2")
+#require("gridExtra")
+
+library("XML")
+library("plyr")
+library("ggplot2")
+library("gridExtra")
+
+fileUrl= "C:/Program Files/R/CountryVulnerabilities/data/official-cpe-dictionary_v2.3.xml"
+doc<-xmlParse(fileUrl)
+rootNode<-xmlRoot(doc)
+rootNode
