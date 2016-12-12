6b129b2691d0842d498f594eba846fc8e70546cb
ilcapone <juanzumbido@hotmail.com>
2016-12-13 00:30:41 +0100

    add: some lines for parsing single xml

--- R/XMLParseCPE.R
+++ R/XMLParseCPE.R
@@ -5,17 +5,19 @@
 #install.packages("ggplot2")
 #install.packages("gridExtra")
 
-#require("XML")
-#require("plyr")
-#require("ggplot2")
-#require("gridExtra")
+require("XML")
+require("plyr")
+require("ggplot2")
+require("gridExtra")
 
-library("XML")
-library("plyr")
-library("ggplot2")
-library("gridExtra")
+#library("XML")
+#library("plyr")
+#library("ggplot2")
+#library("gridExtra")
 
 fileUrl= "C:/Program Files/R/CountryVulnerabilities/data/official-cpe-dictionary_v2.3.xml"
 doc<-xmlParse(fileUrl)
 rootNode<-xmlRoot(doc)
-rootNode
+
+Madhu2012=ldply(xmlToList(fileUrl), data.frame)
+
