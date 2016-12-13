eefe3f1017671ceec4939d4acb1fc1e145379e68
ilcapone <juanzumbido@hotmail.com>
2016-12-13 09:09:38 +0100

    Parse XML to tree structure

--- R/XMLParseCPE.R
+++ R/XMLParseCPE.R
@@ -5,19 +5,27 @@
 #install.packages("ggplot2")
 #install.packages("gridExtra")
 
-require("XML")
-require("plyr")
-require("ggplot2")
-require("gridExtra")
+#require("XML")
+#require("plyr")
+#require("ggplot2")
+#require("gridExtra")
 
-#library("XML")
-#library("plyr")
-#library("ggplot2")
-#library("gridExtra")
+library("XML")
+library("plyr")
+library("ggplot2")
+library("gridExtra")
 
 fileUrl= "C:/Program Files/R/CountryVulnerabilities/data/official-cpe-dictionary_v2.3.xml"
-doc<-xmlParse(fileUrl)
-rootNode<-xmlRoot(doc)
+#doc<-xmlParse(fileUrl)
+treedoc = xmlRoot(xmlTreeParse(fileUrl))
+xmlName(treedoc)
+xmlSApply(treedoc,xmlValue)
 
-Madhu2012=ldply(xmlToList(fileUrl), data.frame)
+#rootNode<-xmlRoot(doc)
+
+#xmlToDataFrame( getNodeSet(doc, "//value"),  colClasses=c("character","numeric"))
+
+#cpe <- ldply(xmlToList(fileUrl), data.frame)
+
+#Madhu2012=ldply(xmlToList(fileUrl), data.frame)
 
