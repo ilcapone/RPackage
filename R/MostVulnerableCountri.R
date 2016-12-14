68662f2d42208963c72044f05135effadd9e54fb
ilcapone <juanzumbido@hotmail.com>
2016-12-15 00:43:51 +0100

    functions to automitace querys

---
+++ R/MostVulnerableCountri.R
@@ -0,0 +1,14 @@
+
+QC_ServiceTipe <- function(Servertipe)
+{
+  httpServerTipe <- paste("80.http.get.headers.server:", Servertipe, sep=" ")
+  httpServerTipe
+}
+
+
+QC_ProtocolandPort <- function(protocol, port)
+{
+  portsandProtocol <- paste("\"",port, "/", protocol,"\"", sep="")
+  protocols <- paste("and protocols:", portsandProtocol, sep=" ")
+  protocols
+}
