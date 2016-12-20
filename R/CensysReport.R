dadfe9ff0b54dbe4589419f2e58a9bd9036d4233
rtorrecillas <rtorrecillas99@gmail.com>
2016-12-20 20:44:37 +0100

    version 2 de la funcion de paises

--- R/CensysReport.R
+++ R/CensysReport.R
@@ -1,31 +1,41 @@
 
-df_countries <- function(protocol = c("443.https")){
-  str_protocol<- protocol
+df_countries <- function(protocol = c("443.https","110.pop3")){
+  protocol<-c("443.https")
   
   if (protocol == "443.https") {
-    library(countrycode)
-    result<-censys_report("ipv4", "443.https.tls.version: \"TLSv1.0\"","location.country_code", 500)
-    df_tls1.0<-result$results
-    
-    result<-censys_report("ipv4", "443.https.tls.version: \"TLSv1.1\"","location.country_code", 500)
-    df_tls1.1<-result$results
-    
-    result<-censys_report("ipv4", "443.https.tls.version: \"TLSv1.2\"","location.country_code", 500)
-    df_tls1.2<-result$results
-    
-    result<-censys_report("ipv4", "443.https.tls.version: \"SSLv3\"","location.country_code", 500)
-    df_sslv3<-result$results
-    
-    df_total<-rbind(df_tls1.0,df_tls1.1,df_tls1.2,df_sslv3)
-    
-    df_total_por_TLS<-dplyr::full_join(df_tls1.0,df_tls1.1,by="key",fill=0)
-    df_total_por_TLS<-dplyr::full_join(df_total_por_TLS,df_tls1.2,by="key",fill=0)
-    df_total_por_TLS<-dplyr::full_join(df_total_por_TLS,df_sslv3,by="key",fill=0)
-    
-    colnames(df_total_por_TLS)<-c("key","TLSv1.0","TLSv1.1","TLSv1.2","SSLv3")
-    
-    df_total_por_TLS<-dplyr::mutate(df_total_por_TLS,Total=ifelse(is.na(TLSv1.0), 0, TLSv1.0)+ifelse(is.na(TLSv1.1), 0, TLSv1.1)+ifelse(is.na(TLSv1.2), 0, TLSv1.2)+ifelse(is.na(SSLv3), 0, SSLv3))  
+    str_protocol<- paste(sep="",protocol,".tls.version:  ")
+  } else if (protocol == "110.pop3") {
+    str_protocol<- paste(sep="",protocol,".starttls.tls.version: ")
   }
+  
+  library(countrycode)
+  str_protocol_use<-paste(sep="",str_protocol,"\"TLSv1.2\"")
+  result<-censys_report("ipv4", str_protocol_use,"location.country_code", 500)
+  df_tls1.2<-result$results
+  
+  str_protocol_use<-paste(sep="",str_protocol,"\"TLSv1.1\"")
+  result<-censys_report("ipv4", str_protocol_use,"location.country_code", 500)
+  df_tls1.1<-result$results
+  
+  str_protocol_use<-paste(sep="",str_protocol,"\"TLSv1.0\"")
+  result<-censys_report("ipv4", str_protocol_use,"location.country_code", 500)
+  df_tls1.0<-result$results
+  
+  str_protocol_use<-paste(sep="",str_protocol,"\"SSLv3\"")
+  result<-censys_report("ipv4", str_protocol_use,"location.country_code", 500)
+  df_sslv3<-result$results
+  
+  df_total<-rbind(df_tls1.0,df_tls1.1,df_tls1.2,df_sslv3)
+  
+  df_total_por_TLS<-dplyr::full_join(df_tls1.0,df_tls1.1,by="key",fill=0)
+  df_total_por_TLS<-dplyr::full_join(df_total_por_TLS,df_tls1.2,by="key",fill=0)
+  df_total_por_TLS<-dplyr::full_join(df_total_por_TLS,df_sslv3,by="key",fill=0)
+  
+  colnames(df_total_por_TLS)<-c("key","TLSv1.0","TLSv1.1","TLSv1.2","SSLv3")
+  
+  df_total_por_TLS<-dplyr::mutate(df_total_por_TLS,Total=ifelse(is.na(TLSv1.0), 0, TLSv1.0)+ifelse(is.na(TLSv1.1), 0, TLSv1.1)+ifelse(is.na(TLSv1.2), 0, TLSv1.2)+ifelse(is.na(SSLv3), 0, SSLv3))  
+  puntuacion<-c(TLSv1.2=10,TLSv1.1=7.5,TLSv1.0=5,SSLv3=2.5)
+  df_total_por_TLS<-dplyr::mutate(df_total_por_TLS,puntuacion=(ifelse(is.na(TLSv1.2), 0, TLSv1.2)*puntuacion["TLSv1.2"]+ ifelse(is.na(TLSv1.1), 0, TLSv1.1)*puntuacion["TLSv1.1"]+ifelse(is.na(TLSv1.0), 0, TLSv1.0)*puntuacion["TLSv1.0"]+ifelse(is.na(SSLv3), 0, SSLv3)*puntuacion["SSLv3"]) /Total)
   
   return(df_total_por_TLS)
 }
